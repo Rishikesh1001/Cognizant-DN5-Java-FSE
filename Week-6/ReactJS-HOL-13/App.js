dfebe3f5b70527999c05b1b2d734f968438101f2
Rishikesh Yadav <152897247+Rishikesh1001@users.noreply.github.com>
2026-07-26 17:34:27 +0530

    Week 6 - ReactJS HOL 13

---
+++ Week-6/ReactJS-HOL-13/App.js
@@ -0,0 +1,12 @@
+import Counter from "./Components/Counter";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Redux Counter App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
